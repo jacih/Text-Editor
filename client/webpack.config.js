21787b14a1b5a948611c29984cfae4ae265b7b8b
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-03-25 06:50:40 -0400

    debug attempts

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -32,11 +29,11 @@ module.exports = () => {
         name: 'A Simple Text Editor',
         description: 'Text-Editor Progressive Web App',
         short_name: 'txed',
-        orientation: "portrait",
+        orientation: 'portrait',
         background_color: '#ffffff',
         theme_color: '#ffffff',
-        start_url: "/",
-        publicPath: "/",
+        start_url: '/',
+        publicPath: '/',
         fingerprints: false,
         inject: true,
         icons: [
